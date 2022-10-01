2a7ff65e24fa4d0a04b56ca3369350b8533a410f
Ayrton <ayrtton.vinicius@gmail.com>
2022-10-01 17:41:03 -0300

    Set the login page style

--- app/Views/login.php
+++ app/Views/login.php
@@ -5,11 +5,10 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Login</title>
+        <link rel="stylesheet" href="<?php echo base_url("css/style.css"); ?>">
     </head>
     <body>
-        <div class="container">
-            <h1>Login</h1>
-            
+        <main>
             <?php if (session()->getFlashdata('msg')) : ?>
                 <div>
                     <?= session()->getFlashdata('msg') ?>
@@ -17,16 +16,21 @@
             <?php endif; ?>
 
             <form action="<?php echo base_url(); ?>/LoginController/login" method="post">
-                <div>
-                    <input type="email" name="email" placeholder="Email">
+                <h1 id="title">Login</h1>
+                <div class="input-container">
+                    <input class="text-input" type="email" name="email" placeholder="Email">
                 </div>
-                <div>
-                    <input type="password" name="password" placeholder="Password">
+                <div class="input-container">
+                    <input class="text-input" type="password" name="password" placeholder="Password">
                 </div>
-                <div>
-                    <button type="submit">Login</button>
+                <div class="input-container">
+                    <button id="button" type="submit">Login</button>
+                </div>
+                <hr />
+                <div id="signup-container">
+                    <a id="signup" href="signup">Create Account</a>
                 </div>
             </form>
-        </div>
+        </main>
     </body>
 </html>
