debac7fe736816c659a5889ea661f0157089a816
Ayrton <ayrtton.vinicius@gmail.com>
2022-10-06 21:15:36 -0300

    Create profile page

--- app/Controllers/ProfileController.php
+++ app/Controllers/ProfileController.php
@@ -6,9 +6,6 @@ use CodeIgniter\Controller;
 
 class ProfileController extends Controller {
     public function index() {
-        $name = session()->get('name');
-        $message =  'Welcome, %s!';
-
-        echo sprintf($message, $name);
+        echo view('profile');
     }
 }
